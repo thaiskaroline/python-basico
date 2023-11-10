3fc522e2e94182749c08d3e1222c7ad8aa62330e
Thais Karoline <108086963+thaiskaroline@users.noreply.github.com>
2023-11-10 14:43:48 -0300

    Add files via upload

---
+++ conversao.py
@@ -0,0 +1,25 @@
+#CONVERSÃO DE TIPOS
+
+idade = '23'
+numero1 = '10'
+numero2 = '15'
+
+print(numero1 + numero2)
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type (idade_inteira))
+
+
+#int()
+#str()
+#float()
+#bool()
+#complex()
+#list()
+#tuple()
+#dict()
+
+
+altura = float(input("INFORME SUA ALTURA"))
+print (altura, type(altura))
